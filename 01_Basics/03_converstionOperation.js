9d30f28f9f7d6ac85bf5f65f5403f44875721518
Lakshay Dhanda <lakshaydh72@gmail.com>
2026-07-05 22:09:43 +0530

    Add files via upload

---
+++ 01_Basics/03_converstionOperation.js
@@ -0,0 +1,71 @@
+let score = "Max-well";
+
+//console.log(typeof (score));
+//console.log(typeof score);
+
+
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);  
+//console.log(valueInNumber);
+
+
+/* "33"=> 33
+ "33abc"=> NaN - Not a number
+ true => 1
+ false => 0
+ null => 0
+ undefined => NaN
+*/
+
+let isLoggedIn = 0;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1=> true , 0 => false
+//"" => false, "Lakshay" => true;
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+//console.log(typeof stringNumber);
+//console.log(stringNumber);
+
+//************Operations*************** */
+
+
+let value = 3;
+let negValue = -value
+//console.log(negValue);
+
+
+// console.log(2+2); // 4
+// console.log(2-2); // 0
+//console.log(2*2); // 4
+// console.log(4/2); // 2
+// console.log(4%3); // 1
+// console.log(2**3); // 8
+
+let str1 = "Lakshay";
+let str2 = "kumar";
+let str3 = str1 + str2;
+// console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+ //console.log(+true);
+ //console.log(+""); 
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
