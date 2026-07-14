e2e757657dde6859010c773ebdc87091f5103802
Lakshay Dhanda <lakshaydh72@gmail.com>
2026-07-14 08:13:41 +0530

    Add files via upload

--- 02_Basics/03_objects.js
+++ 02_Basics/03_objects.js
@@ -1,5 +1,11 @@
+// There are two way to declear object 
+// 1st one is Literals 
+// 2nd one is like constructor 
+
 // singleton
-// Object.create
+// when we cretae the object from constructor. That object is called Singleton.
+// In case of literals. Then singleton not create.
+// [Object.create] This way of creating object is called constructor 
 
 // object literals
 
@@ -7,22 +13,33 @@ const mySym = Symbol("key1")
 
 
 const JsUser = {
-    "full name": "Lakshay",
+    name: "Lakshay",
+   "full name": "Lakshay",
     age: 18,
     [mySym]: "mykey1",
     location: "Jaipur",
     email: "lakshay@QurativeX.com",
     isLoggedIn: false,
-    LastLoginDays: ["Monday", "Sunday"]
+    lastLoginDays: ["Monday", "Sunday"]
 }
 
-console.log(JsUser.email);
-console.log(JsUser['email']);
+console.log(JsUser.email); // this is not the right way.
+console.log(JsUser["email"]);
 console.log(JsUser["full name"]);
 console.log(JsUser[mySym]);
 
+JsUser.email = "lakshay@Chatgpt.com",
+//Object.freeze(JeUser),
 JsUser.email = "lakshay@QurativeX.com",
-Object.freeze(JeUser),
-JsUser.email = "lakshay@QurativeX.com",
-
 console.log(JsUser);
+
+
+JsUser.greeting = function(){
+    console.log("Hello Js user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello Js user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
